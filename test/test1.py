3e56740798ef0b2b8364ff2e6f4ca184aaabc0f2
legolas999 <lqr888888@aliyun.com>
2019-05-04 18:06:51 +0800

    2019-05-04

---
+++ test/test1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.6
+a = input("please input your int numbers: ")
+a = eval(a)
+if a == 0:
+	print('Hello world')
+elif a >0 :
+	print('he\nll\no \nwo\nrl\nd')
+else:
+	for i in 'hello world':
+		print(i)
+
+
+	
